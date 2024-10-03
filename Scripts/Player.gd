1298357b662a5aba8dc483add29b70fa1c36aae0
Sam Schmitz <sms@cfu.net>
2024-10-03 18:21:31 -0500

    animated sprite

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 const speed = 100
+var current_dir = "none"
 
 func _physics_process(delta):
 	player_movement(delta)
@@ -8,19 +9,42 @@ func _physics_process(delta):
 func player_movement(delta):
 	
 	if Input.is_action_pressed("ui_right"):
+		current_dir = "right"
+		play_anim(1)
 		velocity.x = speed
 		velocity.y = 0
 	elif Input.is_action_pressed("ui_left"):
+		current_dir = "left"
+		play_anim(1)
 		velocity.x = -speed
 		velocity.y = 0
 	elif Input.is_action_pressed("ui_up"):
+		current_dir = "up"
+		play_anim(1)
 		velocity.x = 0
 		velocity.y = -speed
 	elif Input.is_action_pressed("ui_down"):
+		current_dir = "down"
+		play_anim(1)
 		velocity.x = 0
 		velocity.y = speed
 	else:
+		play_anim(0)
 		velocity.x = 0
 		velocity.y = 0
 		
 	move_and_slide()
+	
+func play_anim(movement):
+	var dir = current_dir
+	var anim = $AnimatedSprite2D
+	
+	if dir == "left":
+		anim.flip_h = false
+	if dir == "right":
+		anim.flip_h = true
+
+	if movement == 1:
+		anim.play("walking")
+	elif movement == 0:
+		anim.play("idle")
